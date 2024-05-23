860b285e32d61a0cfd63e0adc1f0a2c799a1b2d0
Tunzymono <tife2020@gmail.com>
2024-05-23 14:38:23 +0100

    Styling with styled-components: Created Reusable styled-components components: ControCont, UserInput

---
+++ advance_react/Styling_react_component/starting_project_styling/src/components/ControlCont.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const ControlCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+export default ControlCont
