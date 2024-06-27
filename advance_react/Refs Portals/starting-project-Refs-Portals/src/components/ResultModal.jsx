861e47879e6ca20dfee1d7e4732226a7845fc2ae
Tunzymono <tife2020@gmail.com>
2024-06-27 12:23:41 +0100

    score included and the onClose included in the modal component to invoke a function when ESC button is pressed to close the modal

--- advance_react/Refs Portals/starting-project-Refs-Portals/src/components/ResultModal.jsx
+++ advance_react/Refs Portals/starting-project-Refs-Portals/src/components/ResultModal.jsx
@@ -14,15 +14,22 @@ const ResultModal = forwardRef(function ResultModal(
 
   let lost = timeRemaining <= 0;
   let timeLeft = (timeRemaining/1000).toFixed(2)
+  let score = Math.floor((1-(timeRemaining / (targetTIme*1000)))*100)
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={resetTime}>
       {lost && <h2>You Lost</h2>}
+      {!lost && <h2>You score is {score}</h2>}
       <p>
         The target time was <strong>{targetTIme} seconds.</strong>
       </p>
       <p>
         You stopped the timer with <strong>{timeLeft} seconds left.</strong>
       </p>
+
+      {/* 
+        since we have (a) onClose = {resetTime}, we do not need to specify (b) onSubmit = {resetTime}. (a) will handle it for both
+        but I am leaving it for education sake
+      */}
       <form method="dialog" onSubmit={resetTime}>
         <button>Close</button>
       </form>
