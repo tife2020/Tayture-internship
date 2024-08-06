539d15d1447adb767676ae81282cea0532f385b2
Tunzymono <tife2020@gmail.com>
2024-08-06 10:19:03 +0100

    Consuming the Context Values by the component Shop using and destructuring the values as arguments for the function <CartContext.Consumer>{({products,handleAddItemCart}) => JSX Codes }</CartContext.Consumer>

--- advance_react/ContextAPI_useReducer/01-starting-project/src/components/Shop.jsx
+++ advance_react/ContextAPI_useReducer/01-starting-project/src/components/Shop.jsx
@@ -1,21 +1,24 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context";
 import Product from "./Product";
 
 export default function Shop() {
-  const { products,handleAddItemToCart } = useContext(CartContext)
+  // const { products,handleAddItemToCart } = useContext(CartContext)
   return (
-    
-    <section id="shop">
-      <h2>Elegant Clothing For Everyone</h2>
+    <CartContext.Consumer>
+      {({products,handleAddItemToCart}) => (
+        <section id="shop">
+          <h2>Elegant Clothing For Everyone</h2>
 
-      <ul id="products">
-        {products.map((product) => (
-          <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
-          </li>
-        ))}
-      </ul>
-    </section>
+          <ul id="products">
+            {products.map((product) => (
+              <li key={product.id}>
+                <Product {...product} onAddToCart={handleAddItemToCart} />
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
+    </CartContext.Consumer>
   );
 }
