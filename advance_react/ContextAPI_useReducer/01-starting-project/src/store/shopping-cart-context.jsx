96063f9595c58ff5c0460fcae5178246e78b39b5
Tunzymono <tife2020@gmail.com>
2024-08-12 09:13:01 +0100

    useReducer hook used for state management

--- advance_react/ContextAPI_useReducer/01-starting-project/src/store/shopping-cart-context.jsx
+++ advance_react/ContextAPI_useReducer/01-starting-project/src/store/shopping-cart-context.jsx
@@ -1,9 +1,104 @@
-import { createContext} from "react";
-
+import { createContext, useReducer} from "react";
+import { DUMMY_PRODUCTS } from "../dummy-products.js";
 
 export const CartContext = createContext({
-    items: [],
-    products: [],
-    handleAddItemToCart: () =>{},
-    handleUpdateCartItemQuantity: () => {},
-})
+  items: [],
+  products: [],
+  handleAddItemToCart: () => {},
+  handleUpdateCartItemQuantity: () => {},
+});
+
+function shoppingCartReducer(state, action ) {
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === action.payload
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      const product = DUMMY_PRODUCTS.find(
+        (product) => product.id === action.payload
+      );
+      updatedItems.push({
+        id: action.payload,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+
+    return {
+      items: updatedItems,
+    };
+  }
+
+  if (action.type === "UPDATE_ITEM") {
+    const updatedItems = [...state.items];
+    const updatedItemIndex = updatedItems.findIndex(
+      (item) => item.id === action.payload.productId
+    );
+
+    const updatedItem = {
+      ...updatedItems[updatedItemIndex],
+    };
+
+    updatedItem.quantity += action.payload.amount;
+
+    if (updatedItem.quantity <= 0) {
+      updatedItems.splice(updatedItemIndex, 1);
+    } else {
+      updatedItems[updatedItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+    };
+  }
+}
+
+export default function CartContextProvider({ children }) {
+  const [shoppingCartState, shoppingCartDispatch] = useReducer(
+    shoppingCartReducer,
+    {
+      items: [],
+    }
+  );
+
+  function handleAddItemToCart(id) {
+    shoppingCartDispatch({
+      type: "ADD_ITEM",
+      payload: id,
+    });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    shoppingCartDispatch({
+      type: "UPDATE_ITEM",
+      payload: {
+        productId,
+        amount,
+      },
+    });
+  }
+
+  const contextValues = {
+    items: shoppingCartState.items,
+    products: DUMMY_PRODUCTS,
+    handleAddItemToCart: handleAddItemToCart,
+    handleUpdateCartItemQuantity: handleUpdateCartItemQuantity,
+  };
+
+  return (
+    <CartContext.Provider value={contextValues}>
+      {children}
+    </CartContext.Provider>
+  );
+}
